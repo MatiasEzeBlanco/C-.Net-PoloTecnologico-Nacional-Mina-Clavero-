883c5d692b0a89c8ea82565fd139fed842709d75
Matias Blanco <105614554+MatiasEzeBlanco@users.noreply.github.com>
2022-06-08 03:19:40 -0300

    Add files via upload

---
+++ Clase4 Condicionales/Clase4 Condicionales/Program.cs
@@ -0,0 +1,49 @@
+﻿//Clase 3----------------------------------
+
+//Console.Write("Ingrese su edad:");
+//int edad = int.Parse(Console.ReadLine());
+
+//if (edad < 12)
+//{
+//    Console.WriteLine("Va a la primaria");
+//}
+//else if (edad < 18)
+//{
+//    Console.WriteLine("Va a la secundaria");
+//}
+//else if(edad < 28)
+//{
+//    Console.WriteLine("Va a la facultad");
+//}
+//else
+//{
+//    Console.WriteLine("Va a trabajar");
+//}
+
+//Ejemplo de switch
+
+//Clase 4---------------------------------
+
+//Console.WriteLine("1-Suma");
+//Console.WriteLine("2-Resta");
+//Console.WriteLine("3-Multiplicacion");
+//Console.WriteLine("4-Division");
+//string operacion = Console.ReadLine();
+
+//switch (operacion)
+//{
+//    case "1":
+//        Console.WriteLine("Sumando...");
+//        break;
+//        case "2":
+//        Console.WriteLine("Restando...");
+//        break ;
+//        case "3":
+//        Console.WriteLine("Multiplicando...");
+//        break;
+//        case "4":
+//        Console.WriteLine("Dividiendo...");
+//        break;
+//        default: Console.WriteLine("Ingrese una opcion valida");
+//        break;
+//}
