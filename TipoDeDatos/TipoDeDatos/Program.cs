7efd24341a0310cfc255755b5d6510f57da83baa
Matias Blanco <105614554+MatiasEzeBlanco@users.noreply.github.com>
2022-06-08 03:17:22 -0300

    Add files via upload

---
+++ TipoDeDatos/TipoDeDatos/Program.cs
@@ -0,0 +1,12 @@
+﻿// Clase 1 
+Console.WriteLine("Hello, World!");
+Console.Clear();
+
+Console.WriteLine("Escriba su nombre");
+string nombre;
+nombre = Console.ReadLine();
+Console.Write("Hola " + nombre + " este es mi segundo programa ");
+Console.WriteLine("Escribe tu apellido");
+string apellido;
+apellido = Console.ReadLine();
+Console.WriteLine("Correcto " + nombre + " " + apellido + " Muchas gracias");
